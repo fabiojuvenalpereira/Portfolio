1ef16f8950d1900c0aebe2bed1c9595e58ab6129
fabio <fabiojuvenalpereira@hotmail.com>
2022-11-14 21:09:40 -0300

    feat: add global style

---
+++ src/globalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+      box-sizing: 0;
+      border: none;
+      margin: 0;
+      outline: none;
+      padding: 0;
+    }
+`;
+
+export default GlobalStyle;
--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,12 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './globalStyle';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <GlobalStyle />
   </BrowserRouter>,
   document.getElementById('root'),
 );
