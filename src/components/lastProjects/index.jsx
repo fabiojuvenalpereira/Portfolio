4e71a5f000934e83ed8928872d4a8dbaf2f741ac
fabio <fabiojuvenalpereira@hotmail.com>
2022-11-19 23:32:48 -0300

    feat: add last projects component

---
+++ src/components/lastProjects/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './style';
+
+function LastProjects() {
+  return (
+    <Main>
+      <div>
+        <h1 className="title">PROJETOS RECENTES</h1>
+      </div>
+    </Main>
+  );
+}
+
+export default LastProjects;
---
+++ src/components/lastProjects/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Main = styled.main`
+  background-color: #181818;
+  clip-path: polygon(0 10%, 100% 0, 100% 100%, 0 100%);
+  height: 100%;
+
+  .title {
+    color: white;
+    text-align: center;
+    padding-top: 3em;
+  }
+`;
+
+export default Main;
