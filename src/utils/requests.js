47b9984cdaa28fc5b9d4d48bd41611938e6ec0ac
fabio <fabiojuvenalpereira@hotmail.com>
2022-12-09 00:28:53 -0300

    feat: add function to make requests

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3001' || process.env.REACT_URL;
 
 export async function getProjects() {
-  const response = await axios.get(`${BASE_URL}/projetos`);
+  const response = await axios.get(`${BASE_URL}/projects`);
   return response.data;
 }
 
 // esqueleto da função
 export async function addProjects(data) {
-  const response = await axios.post(`${BASE_URL}/projetos`, data);
-  return response.status;
+  console.log(data);
+  // const response = await axios.post(`${BASE_URL}/projects`, data);
+  // return response.status;
 }
